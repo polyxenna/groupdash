150f86e87e1edf0a7e4099ead811b20518a94dc5
Bianca Jessa A. Carabio <89141786+polyxenna@users.noreply.github.com>
2023-11-10 18:51:28 +0800

    update

--- dashboard.py
+++ dashboard.py
@@ -1,58 +1,55 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
-from datetime import datetime
+import streamlit as st
 
 # Load the dataset
-data = pd.read_csv('group.csv')
+df = pd.read_csv('group8.csv')
 
-# Convert the 'Date' column to a datetime format
-data['Date'] = pd.to_datetime(data['Date'])
+# Filter the dataset for the specified members (Bianca, Genevieve, John)
+members = ['Bianca', 'Genevieve', 'John']
 
-# Filter the data for rows where 'Activity' is 'Sleep' or 'Sleeping'
-sleep_data = data[data['Activity'].isin(['Sleep', 'Sleeping'])]
+# Create a list to store the individual graphs
+individual_graphs = []
 
-# Extract and parse the 'Time' column as a string in the format '%I:%M:%S %p'
-sleep_data['Time'] = pd.to_datetime(sleep_data['Time'], format='%I:%M:%S %p').dt.strftime('%I:%M %p')
+# Create separate graphs for each member
+for member in members:
+    # Filter data for the current member
+    df_member = df[df['Member'] == member]
 
-# Group the data by 'Date' and calculate the average 'Time' for each day
-average_sleep_start = sleep_data.groupby('Date')['Time'].min()
+    # Group by Activity Type and calculate the total duration in hours
+    grouped_data = df_member.groupby('Activity Type')['Duration'].sum() / 60.0  # Convert minutes to hours
+    grouped_data = grouped_data.reset_index()
 
-# Reset the index to make 'Date' a column
-average_sleep_start = average_sleep_start.reset_index()
+    # Sort the data by the total duration in descending order
+    sorted_data = grouped_data.sort_values(by='Duration', ascending=False)
 
-# Calculate the average sleep duration
-average_sleep_duration = sleep_data.groupby('Date')['Duration'].mean() / 60  # Convert minutes to hours
+    # Create a bar graph using plotly for the current member
+    fig = px.bar(sorted_data, x='Activity Type', y='Duration',
+                 title=f'Total Duration of Activity Types for {member} (in hours)',
+                 labels={'Duration': 'Total Duration (hours)'}, color='Activity Type')
 
-# Reset the index to make 'Date' a column
-average_sleep_duration = average_sleep_duration.reset_index()
+    # Add the current graph to the list
+    individual_graphs.append(fig)
 
-# Create a line plot for average sleep duration using Plotly
-fig_duration = px.line(average_sleep_duration, x='Date', y='Duration', labels={'x': 'Date', 'y': 'Average Duration (hours)'})
-fig_duration.update_xaxes(
-    tickvals=average_sleep_duration['Date'],
-    ticktext=average_sleep_duration['Date'].dt.strftime('%m/%d/%Y'),
-    title_text='Date'
-)
+# Create a combined graph for all members
+combined_data = df.groupby('Activity Type')['Duration'].sum() / 60.0  # Convert minutes to hours
+combined_data = combined_data.reset_index()
+combined_sorted_data = combined_data.sort_values(by='Duration', ascending=False)
 
-# Create a line plot for average sleep start time using Plotly
-fig_start_time = px.line(average_sleep_start, x='Date', y='Time', labels={'x': 'Date', 'y': 'Average Sleep Start Time'})
-fig_start_time.update_xaxes(
-    tickvals=average_sleep_start['Date'],
-    ticktext=average_sleep_start['Date'].dt.strftime('%m/%d/%Y'),
-    title_text='Date'
-)
-fig_start_time.update_yaxes(
-    tickvals=average_sleep_start['Time'].unique(),
-    ticktext=average_sleep_start['Time'],
-    title_text='Average Sleep Start Time'
-)
+# Create a bar graph using plotly for the combined data
+combined_fig = px.bar(combined_sorted_data, x='Activity Type', y='Duration',
+                      title='Total Duration of Activity Types Across All Members (in hours)',
+                      labels={'Duration': 'Total Duration (hours)'}, color='Activity Type')
 
-# Streamlit app
-st.title('Group 8 Dashboard')
+# Create a pie chart to show the distribution of time spent in different locations
+location_data = df.groupby('Location')['Duration'].sum() / 60.0  # Convert minutes to hours
+location_data = location_data.reset_index()
+location_fig = px.pie(location_data, names='Location', values='Duration',
+                      title='Distribution of Time Spent in Different Locations (in hours)')
 
-st.write("### Average Sleep Duration per Day")
-st.plotly_chart(fig_duration)
+# Display the individual graphs, the combined graph, and the pie chart in Streamlit
+for i, graph in enumerate(individual_graphs):
+    st.plotly_chart(graph, key=f'Graph {i+1}')
 
-st.write("### Average Sleep Start Time per Day")
-st.plotly_chart(fig_start_time)
+st.plotly_chart(combined_fig, key='Combined Graph')
+st.plotly_chart(location_fig, key='Location Pie Chart')
